5390e6f6df07152921bd135a086a269f0460d3d6
pcoloigner <philippe.coloigner@gmail.com>
2020-02-20 05:00:11 +0100

    initial commit

---
+++ src/main/java/org/pcomeziantou/planningsds/PlanningsdsApplication.java
@@ -0,0 +1,13 @@
+package org.pcomeziantou.planningsds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanningsdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanningsdsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/pcomeziantou/planningsds/PlanningsdsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pcomeziantou.planningsds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanningsdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
